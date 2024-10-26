7f3fb7c9ea1724478455037e7f1c2f04bafb5d08
Jpeels2811 <jos.herrera@duocuc.cl>
2024-10-26 17:47:55 -0300

    Actualizacion de nombres y orden

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,29 +5,28 @@ import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 const routes: Routes = [
   {
     path: 'home',
-    loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)
+    loadChildren: () => import('./Home/home.module').then( m => m.HomePageModule)
   },
   {
-    path: '',
-    redirectTo: 'home',
-    pathMatch: 'full'
+    path: '', 
+    redirectTo: 'home', pathMatch: 'full'
   },
 
   {
     path: 'register',
-    loadChildren: () => import('./pages/register/register.module').then( m => m.RegisterPageModule)
+    loadChildren: () => import('./pages/Acceso/Register/register.module').then( m => m.RegisterPageModule)
   },
   {
     path: 'login',
-    loadChildren: () => import('./pages/login/login.module').then( m => m.LoginPageModule)
+    loadChildren: () => import('./pages/Acceso/Login/login.module').then( m => m.LoginPageModule)
   },
   {
     path: 'main-page',
-    loadChildren: () => import('./pages/main-page/main-page.module').then( m => m.MainPagePageModule)
+    loadChildren: () => import('./pages/Page-Main/main-page.module').then( m => m.MainPagePageModule)
   },
   {
     path: 'recover-password',
-    loadChildren: () => import('./pages/recover-password/recover-password.module').then( m => m.RecoverPasswordPageModule)
+    loadChildren: () => import('./pages/Acceso/Recuperar-Pass/recover-password.module').then( m => m.RecoverPasswordPageModule)
   },
 ];
 
